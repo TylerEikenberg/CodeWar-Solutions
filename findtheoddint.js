05c419b022f3f57ec4746c4c1a97771a62203463
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-22 14:12:51 -0500

    findoddint

---
+++ findtheoddint.js
@@ -0,0 +1,17 @@
+function findOdd(A) {
+  // store number that appears odd number of times
+  // iterate through array
+  //      keep count of each element
+  //          pull out number that appears an odd number of times
+  const counts = {};
+
+  A.forEach(element => {
+    counts[element] = (counts[element] || 0) + 1;
+  });
+
+  for (let [key, value] of Object.entries(counts)) {
+    if (value % 2 !== 0) {
+      return parseInt(key);
+    }
+  }
+}
